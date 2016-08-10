89bfd42eb7783d93ed0f0dcb8c606beb1e0b33f0
dell <9716588439vik@gmail.com>
2016-08-10 07:16:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/cardstackview/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.dell.cardstackview;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.dell.myboom.AllMoveDownAnimatorAdapter;
+import com.example.dell.myboom.CardStackView;
+import com.example.dell.myboom.UpDownAnimatorAdapter;
+import com.example.dell.myboom.UpDownStackAnimatorAdapter;
+
+import java.util.Arrays;
+@SuppressWarnings("deprecation")
+public class MainActivity extends AppCompatActivity {
+    public static Integer[] TEST_DATAS = new Integer[]{
+            R.color.color_1,
+            R.color.color_2,
+            R.color.color_3,
+            R.color.color_4,
+            R.color.color_5,
+            R.color.color_6,
+            R.color.color_7,
+            R.color.color_8,
+            R.color.color_9,
+            R.color.color_10,
+            R.color.color_11,
+            R.color.color_12,
+            R.color.color_13,
+            R.color.color_14,
+            R.color.color_15,
+            R.color.color_16,
+            R.color.color_17,
+            R.color.color_18,
+            R.color.color_19,
+            R.color.color_20,
+            R.color.color_21,
+            R.color.color_22,
+            R.color.color_23,
+            R.color.color_24,
+            R.color.color_25,
+            R.color.color_26
+    };
+    private CardStackView mStackView;
+    private TestStackAdapter mTestStackAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStackView = (CardStackView) findViewById(R.id.stackview_main);
+        mTestStackAdapter = new TestStackAdapter(this);
+        mStackView.setAdapter(mTestStackAdapter);
+
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        mTestStackAdapter.updateData(Arrays.asList(TEST_DATAS));
+                    }
+                }
+                , 200
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_all_down:
+                mStackView.setAnimatorAdapter(new AllMoveDownAnimatorAdapter(mStackView));
+                break;
+            case R.id.menu_up_down:
+                mStackView.setAnimatorAdapter(new UpDownAnimatorAdapter(mStackView));
+                break;
+            case R.id.menu_up_down_stack:
+                mStackView.setAnimatorAdapter(new UpDownStackAnimatorAdapter(mStackView));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
